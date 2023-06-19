b4d2da0184a83c181412a44f0915e54b90f49906
TranNgocQuy123 <quyxe1998@gmail.com>
2023-06-19 19:20:48 +0700

    update quytn11

--- app.py
+++ app.py
@@ -4,28 +4,6 @@ import logging
 
 app = Flask(__name__)
 
-@app.route('/status')
-def healthcheck():
-    response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
-    )
-
-    app.logger.info('Status request successfull')
-    return response
-
-@app.route('/metrics')
-def metrics():
-    response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
-    )
-
-    app.logger.info('Metrics request successfull')
-    return response
-
 @app.route("/")
 def hello():
     app.logger.info('Main request successfull')
