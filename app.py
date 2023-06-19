0c84dbd61a238859b4ab5aa8970259270ce8af9f
TranNgocQuy123 <quyxe1998@gmail.com>
2023-06-19 19:33:39 +0700

    update quytn11

--- app.py
+++ app.py
@@ -1,17 +1,12 @@
 from flask import Flask
-from flask import json
 import logging
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    app.logger.info('Main request successfull')
-
     return "Hello QuyTN7! Welcome to Capstone Udacity"
 
 if __name__ == "__main__":
     ## stream logs to a file
-    logging.basicConfig(filename='app.log',level=logging.DEBUG)
-
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=80, debug=True)  
